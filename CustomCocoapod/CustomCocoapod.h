79d1d64959020fc5937709430ca5048ef6514c57
Jimoh Babatunde <jimoh.abdullahi@konga.com>
2020-01-07 13:05:19 +0100

    First Commit

---
+++ CustomCocoapod/CustomCocoapod.h
@@ -0,0 +1,19 @@
+//
+//  CustomCocoapod.h
+//  CustomCocoapod
+//
+//  Created by Jimoh Babatunde  on 07/01/2020.
+//  Copyright © 2020 Jimoh Babatunde. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for CustomCocoapod.
+FOUNDATION_EXPORT double CustomCocoapodVersionNumber;
+
+//! Project version string for CustomCocoapod.
+FOUNDATION_EXPORT const unsigned char CustomCocoapodVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CustomCocoapod/PublicHeader.h>
+
+
---
+++ CustomCocoapod/Service.swift
@@ -0,0 +1,18 @@
+//
+//  Service.swift
+//  CustomCocoapod
+//
+//  Created by Jimoh Babatunde  on 07/01/2020.
+//  Copyright © 2020 Jimoh Babatunde. All rights reserved.
+//
+
+import Foundation
+
+public class Service {
+    
+    private init () {}
+    
+    public static func displayMessage() -> String {
+        return "Welcome to my super services."
+    }
+}
